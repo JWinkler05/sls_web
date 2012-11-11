e8765e4c19f0220fdeb0a30e396f8c08b2a3dce4
Dev from web1 <web1.smartlocalsocial.com>
2012-11-11 02:52:33 +0000

    enabled minify and fixed whitespace

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -116,8 +116,9 @@ Kohana::modules(array(
 	// 'image'      => MODPATH.'image',      // Image manipulation
 	// 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
 	// 'unittest'   => MODPATH.'unittest',   // Unit testing
-	'userguide'  => MODPATH.'userguide',  // User guide and API documentation
-	'storage'    => MODPATH.'storage',
+	'userguide'  => MODPATH.'userguide',     // User guide and API documentation
+	'storage'    => MODPATH.'storage',       // Storage Driver (rackspace)
+	'minify'     => MODPATH.'minify',        // Minifier
 	));
 
 /**
