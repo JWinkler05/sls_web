f8ea9e4f7b42a76a2a939988aa948266e7c8e52d
Dev from web1 <web1.smartlocalsocial.com>
2012-11-21 04:43:34 +0000

    minify config

---
+++ modules/tpl-cityscape/config/minify.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+  'groupsConfig' => array(
+	'js' => array(
+		'//media/tpl-cityscape/js/adapt.min.js',
+	),
+        'css' => array(
+                '//media/tpl-cityscape/css/container.css',
+                '//media/tpl-cityscape/css/header.css',
+                '//media/tpl-cityscape/css/main_menu.css',
+                '//media/tpl-cityscape/css/content.css',
+		'//media/tpl-cityscape/css/960.min.css',
+        ),
+   )
+);
+
