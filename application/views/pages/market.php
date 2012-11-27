dd8ff973badbab33b84bc29620f30b3838495322
Dev from web1 <web1.smartlocalsocial.com>
2012-11-27 03:22:27 +0000

    added market section

---
+++ application/views/pages/market.php
@@ -0,0 +1,2 @@
+<div class="note">If you are looking for another market: <a class="note-link" href="http://www.getdeals.mobi">click here</a></div>
+<div class="market-intro">Current market: <span class="market">Columbus, OH</span></div>
--- application/views/template/cityscape/components/header.php
+++ application/views/template/cityscape/components/header.php
@@ -1,5 +1,7 @@
 <div id="cp_header" class="container_16">
-	<div id ="cp_main_logo" class="grid_5"><?php echo View::factory('pages/main_logo')->render(); ?></div>
-	<div id = "cp_main_menu" class="grid_11"><?php if ($menu){ echo View::factory('pages/main_menu')->render();} ?></div>
+	<div id = "cp_main_market" class="grid_16"><?php echo View::factory('pages/market')->render(); ?></div>
+	<div id = "cp_main_logo" class="grid_5"><?php echo View::factory('pages/main_logo')->render(); ?></div>
+	<div id = "cp_main_menu" class="grid_11"><?php if ($menu) { echo View::factory('pages/main_menu')->render(); } ?>
+	</div>
 </div>
 
