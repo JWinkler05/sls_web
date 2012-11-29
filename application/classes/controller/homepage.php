ec05f262fa5845b3bd1906fbf635e85d527f09ec
Dev from web1 <web1.smartlocalsocial.com>
2012-11-29 05:59:01 +0000

    removed uneeded code

--- application/classes/controller/homepage.php
+++ application/classes/controller/homepage.php
@@ -26,25 +26,4 @@ class Controller_Homepage extends Controller_Template_Cityscape_Default
 		$this->template->content = $view;
 	}
 	
-	public function action_deal()
-	{
-		View::set_global('menu',TRUE);
-		$view = View::factory('pages/home');
-
-		$view->ads_primary = json_decode(Request::factory('ads_primary/index')->execute());
-		$view->ads_secondary = json_decode(Request::factory('ads_secondary/index')->execute());	
-
-		$view->main_grid = '16';
-		$view->main_layout = 'no-sidebar';
-
-		if (is_array($view->ads_secondary)) {
-			$view->main_grid = '12';
-			$view->main_layout = 'sidebar';
-		}
-
-		//$this->response->body($view);
-		$this->template->title = __('Welcome to smartlocalsocial.com');
-		
-		$this->template->content = $view;
-	}
 }
