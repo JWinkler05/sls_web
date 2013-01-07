5a6e651b3c456c8a5a84f4e5172ca3cd0dd3fcca
Dev from web1 <web1.smartlocalsocial.com>
2013-01-07 02:17:47 +0000

    restructured for readability

--- application/views/pages/status.php
+++ application/views/pages/status.php
@@ -1,12 +1,12 @@
-<?php
-if (Auth::instance()->logged_in()) { ?>
-Welcome <?php echo Auth::instance()->get_user()->user.' '; 
-	if (Auth::instance()->logged_in('admin')) { ?>
-* You have admin access *
-<?php	}
-echo HTML::anchor('admin','Control Panel'). ' - ';
-echo HTML::anchor('session/logout','Logout');
+<?php 
+if (Auth::instance()->logged_in()) {
+	echo 'Welcome '.Auth::instance()->get_user()->user.' '; 
+	if (Auth::instance()->logged_in('admin')) { 
+		echo '- You have admin access -';
+		echo HTML::anchor('admin','Control Panel'). ' - ';
+	}
+	echo HTML::anchor('session/logout','Logout');
 } else {
-echo HTML::anchor('session/login','Login');
+	echo HTML::anchor('session/login','Login');
 }
 ?>
