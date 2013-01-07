b25f9e42049c1c29ee178e5d0c4709bd1b6bfd20
Dev from web1 <web1.smartlocalsocial.com>
2013-01-07 05:29:42 +0000

    added admin creatives edit

--- application/classes/controller/admin/creative/edit.php
+++ application/classes/controller/admin/creative/edit.php
@@ -7,17 +7,12 @@ class Controller_Admin_Creative_Edit extends Controller_Template_Cityscape_Defau
 		$params = $this->request->query();
 		$id = $params['id'];
 
-		View::set_global('menu',TRUE);
-		$view = View::factory('pages/deal_detail');
+		$view = View::factory('pages/admin/creative/edit');
 
-		$detail_return = json_decode(Request::factory('ads_detail/index')->query('di',$id)->execute());
+		$detail_return = json_decode(Request::factory('ads_detail/index')->query('id',$id)->execute());
 		$detail = $detail_return->results['0']->creative;
 		$view->selected = $detail;
 		$view->id = $id; 
-		$ads_primary = json_decode(Request::factory('ads_primary/index')->execute());
-		$view->ads = $ads_primary->results; 
-		$view->main_grid = '16';
-		$view->main_layout = 'no-sidebar';		
 		//$this->response->body($view);
 		$this->template->title = __('smartlocalsocial.com: ' . $detail->business_name . $detail->offer_in_short);
 		
---
+++ application/views/pages/admin/creative/edit.php
@@ -0,0 +1,145 @@
+<div id="cp_main_content" class="grid_16 no-sidebar">
+	<div class="grid_6">
+		<p>Edit Details</p><br/>
+<?php
+$attr = array('width' => '23px');
+echo $form = Formo::form()
+	->add(
+		'creative_id',
+		array(
+			'value'=>$selected->creative_id,
+			'editable'=>FALSE,
+			$attr
+		)
+	)
+	->add(
+		'ad_type',
+		array(
+			'value'=>$selected->ad_type,
+			$attr
+		)
+	)
+	->add(
+		'business_name',
+		array(
+			'value'=>$selected->business_name,
+			$attr
+		)
+	)
+	->add(
+		'offer_in_short',
+		array(
+			'value'=>$selected->offer_in_short,
+			$attr
+		)
+	)
+	->add(
+		'list_price',
+		array(
+			'value'=>$selected->list_price,
+			$attr
+		)
+	)
+	->add(
+		'coupon_price',
+		array(
+			'value'=>$selected->coupon_price,
+			$attr
+		)
+	)
+	->add(
+		'sms_code',
+		array(
+			'value'=>$selected->sms_code,
+			$attr
+		)
+	)
+	->add(
+		'description',
+		'textarea',
+		array(
+			'value'=>$selected->description,
+			$attr
+		)
+	)
+	->add(
+		'detailed_offer',
+		'textarea',
+		array(
+			'value'=>$selected->detailed_offer,
+			$attr
+		)
+	)
+	->add(
+		'submit',
+		'submit',
+		array(
+			'value'=>'Submit Changes',
+			$attr
+		)
+	);
+if ($form->load($_POST)->validate())
+{
+/* Add saving logic here */
+}
+?>
+	</div>
+	<p>Single Column Ad preview</p>
+	<div class="grid_4 ad_single">
+		<div class="ad_type"><?php echo $selected->ad_type ?></div>
+		<img class="ad_image" src="<?php echo "/media/ad_images/{$selected->ad_image_name}" ?>"/>
+		<div class="ad_body">
+			<div class="ad_business_name"><?php echo $selected->business_name; ?></div>
+			<div class="ad_offer_short"><?php echo $selected->offer_in_short; ?></div>
+			<div class="ad_list_price"><?php echo $selected->list_price; ?></div>
+			<div class="ad_coupon_price"><?php echo $selected->coupon_price; ?></div>
+			<div class="ad_sms_code"><?php echo $selected->sms_code; ?></div>
+		</div>
+	</div>
+	<div class="grid_16 outergray">
+	<p>Full Size Detail preview</p>
+		<div class="innerwhite">
+			<div class="column grid_4 detail_offset">
+				<h2><?php echo $selected->business_name; ?></h2>
+				<p><?php echo $selected->description; ?></p>
+				<p><?php echo $selected->sms_code; ?></p>
+			</div>
+			<div class="mainimg grid_8">
+				<img width="450" height="328" alt="image" src="<?php echo "/media/ad_images/{$selected->ad_image_name}" ?>">
+			</div>
+			<div class="columnafter grid_4 detail_offset">
+				<p>Offer Details</p>
+				<p class="advdeal"><?php echo $selected->detailed_offer; ?></p>
+				<p>Regular Price</p>
+				<p class="regprice"><?php echo $selected->list_price; ?></p>
+				<p>Sale Price</p>
+				<p class="saleprice"><?php echo $selected->coupon_price; ?></p>
+			
+			</div>
+		</div>
+	</div>
+		<?php
+/* 
+echo '<br/>'.$selected->id;
+echo '<br/>'.$selected->ad_type;
+echo '<br/>'.$selected->ad_size;
+echo '<br/>'.$selected->image_location;
+echo '<br/>'.$selected->location_specific;
+echo '<br/>'.$selected->location_city;
+echo '<br/>'.$selected->business_name;
+echo '<br/>'.$selected->offer_in_short;
+echo '<br/>'.$selected->list_price;
+echo '<br/>'.$selected->coupon_price;
+echo '<br/>'.$selected->sms_code;
+echo '<br/>'.$selected->category_id;
+echo '<br/>'.$selected->specific_category;
+echo '<br/>'.$selected->savings;
+echo '<br/>'.$selected->expiration;
+echo '<br/>'.$selected->description;
+echo '<br/>'.$selected->detailed_offer;
+echo '<br/>'.$selected->ad_image_name;
+echo '<br/>'.$selected->company_logo_image_name;
+*/
+		?>
+
+</div>
