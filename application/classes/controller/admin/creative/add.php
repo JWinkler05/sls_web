4de9c0d6bd733b2bbf42763446ff543bbfc23284
Dev from web1 <web1.smartlocalsocial.com>
2013-01-16 02:05:15 +0000

    refactored add creative page

--- application/classes/controller/admin/creative/add.php
+++ application/classes/controller/admin/creative/add.php
@@ -4,23 +4,73 @@ class Controller_Admin_Creative_Add extends Controller_Template_Cityscape_Defaul
 {
 	public function action_index()
 	{
-		$params = $this->request->query();
-		$id = $params['di'];
+		// Grab the post variables
+		$posted = $this->request->post();
 
-		View::set_global('menu',TRUE);
-		$view = View::factory('pages/deal_detail');
+		// Create Blank Ad Object
+		$creative = new stdClass;
+		$creative->ad_type = NULL;
+		$creative->ad_image_name = NULL;
+		$creative->business_name = NULL;
+		$creative->offer_in_short = NULL;
+		$creative->list_price = NULL;
+		$creative->coupon_price = NULL;
+		$creative->sms_code = NULL;
+		$creative->description = NULL;
+		$creative->detailed_offer = NULL;
 
-		$detail_return = json_decode(Request::factory('ads_detail/index')->query('di',$id)->execute());
-		$detail = $detail_return->results['0']->creative;
-		$view->selected = $detail;
-		$view->id = $id; 
-		$ads_primary = json_decode(Request::factory('ads_primary/index')->execute());
-		$view->ads = $ads_primary->results; 
-		$view->main_grid = '16';
-		$view->main_layout = 'no-sidebar';		
-		//$this->response->body($view);
-		$this->template->title = __('smartlocalsocial.com: ' . $detail->business_name . $detail->offer_in_short);
+		// Create Blank Form
+		$form = Formo::form()
+			->add('ad_type')
+			->add('business_name')
+			->add('offer_in_short')
+			->add('list_price')
+			->add('coupon_price')
+			->add('sms_code')
+			->add('description','textarea')
+			->add('detailed_offer','textarea')
+			->add('submit','submit',array('value'=>'Submit Changes'));
+
+		// Logic to execute on a successful post
+		if ($form->load($_POST)->validate())
+		{
+			// Remove submit field if present in the post variables
+			$fields = array ('fields' => $posted);
+			unset($fields['fields']['submit']);
+
+			// Set API server based on environment
+			if (Kohana::$environment === Kohana::PRODUCTION){
+				$api_server = 'api.smartlocalsocial.com';
+			}else{
+				$api_server = 'devapi.smartlocalsocial.com';
+			}
+
+			// Execute put request to create record
+			//$request = Request::factory("http://$api_server/creatives/{$detail->creative_id}")
+			//	->method(Request::PUT)
+			//	->body(json_encode($fields))
+			//	->headers('Content-Type', 'application/json');
 		
+			//$response = $request->execute();
+
+			// Parse response for creative id
+			$creative_id = 'abc';
+
+			// Redirect to edit page
+			$this->request->redirect('/admin_creative_edit?id='.$creative_id); 
+		}
+
+		// Create view for the edit page
+		$view = View::factory('pages/admin/creative/edit');
+
+		// Assign variables to be accessed in the view
+		$view->form = $form;
+		$view->creative = $creative;
+
+		// Set over page title in template
+		$this->template->title = __('smartlocalsocial.com: Add new creative');
+		
+		// Display view in template
 		$this->template->content = $view;
 	}
 	
