14616c835ad10e8b24efb53a99c1056b27a81743
Dev from web1 <web1.smartlocalsocial.com>
2013-01-17 04:38:49 +0000

    mongo config

---
+++ application/config/mongo.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * See http://www.php.net/manual/en/mongo.construct.php for configuration options.
+ */
+return array(
+  'default' => array(
+    'database'  => 'geoip',
+    'server'  => 'mongodb://testUser:testPass@datastore.smartlocalsocial.com/geoip',
+    'options'   => array(),
+    'profiling' => FALSE
+  )
+);
