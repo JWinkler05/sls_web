caf35ecfeb41b7bd90a2919858aadf59525d63a0
Dev from web1 <web1.smartlocalsocial.com>
2013-02-03 19:17:47 +0000

    moved ads detail to restcall

--- application/classes/controller/ads/detail.php
+++ application/classes/controller/ads/detail.php
@@ -4,15 +4,9 @@ class Controller_Ads_Detail extends Controller
 {
 	public function action_index()
 	{
-		if (Kohana::$environment === Kohana::PRODUCTION){
-			$api_server = 'api.smartlocalsocial.com';
-		}else{
-			$api_server = 'devapi.smartlocalsocial.com';
-		}
-		
 		$params = $this->request->query();
 
-		$hmvc = Request::factory("http://$api_server/creatives/get_details_by_id?id={$params['id']}")
+		$hmvc = Request::factory('http://'.Servers::$api_server."/creatives/{$params['id']}")
 			->execute();
 
 		$this->response->body($hmvc);
