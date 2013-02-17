2684422f920a852449ea4cb42e7ec9034cf59ab1
John <jwinkler@John-SLS.(none)>
2013-02-17 11:31:09 -0500

    Changes to detail page layout and formatting.

--- application/views/pages/deal_detail.php
+++ application/views/pages/deal_detail.php
@@ -1,24 +1,28 @@
 <div id="cp_main_content" class="grid_<?php echo $main_grid.' '.$main_layout;?>">
 	<div class="grid_16 outergray">
-		<div class="innerwhite">
-			<div class="column grid_4 detail_offset">
-				<h2><?php echo $selected->business_name; ?></h2>
-				<p><?php echo $selected->description; ?></p>
-				<p><?php echo $selected->sms_code; ?></p>
-			</div>
-			<div class="mainimg grid_8">
-				<img width="450" height="328" alt="image" src="<?php echo "/media/ad_images/{$detail_image->local_location}" ?>">
-			</div>
-			<div class="columnafter grid_4 detail_offset">
-				<p>Offer Details</p>
-				<p class="advdeal"><?php echo $selected->detailed_offer; ?></p>
-				<p>Regular Price</p>
-				<p class="regprice"><?php echo $selected->list_price; ?></p>
-				<p>Sale Price</p>
-				<p class="saleprice"><?php echo $selected->coupon_price; ?></p>
-			
-			</div>
-		</div>
+            <div class="innerwhite">
+                <div class="mainimg grid_8">
+                    <center><h1 class="buistitle"><?php echo $selected->business_name; ?></h1></center>
+                    <img width="450" height="328" alt="image" src="<?php echo "/media/ad_images/{$detail_image->local_location}" ?>">
+                </div>
+                <div class="column grid_4 detail_offset">
+                    <p align="center"><?php echo $selected->description; ?></p>
+                    <p class="sms_blurb"><br /><?php echo "For more great deals from " . $selected->business_name . ", "; ?></p>
+                    <p class ="sms_code"><?php echo $selected->sms_code; ?></p>
+                    <br/>
+                    <p><strong>Offer Details</strong></p>
+                    <p class="advdeal"><?php echo $selected->detailed_offer; ?></p>
+                </div>
+                <div class="columnafter grid_4 detail_offset">
+                    <p><strong>Regular Price</strong></p>
+                    <p class="regprice"><?php echo "$" . $selected->list_price; ?></p>
+                    <br/>
+                    <p><strong>Sale Price</strong></p>
+                    <p class="saleprice"><?php echo "$" . $selected->coupon_price; ?></p>
+
+                </div>
+            </div>
+
 	</div>
 		<?php
 /* 
