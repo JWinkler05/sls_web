53bc8a78dc4e9e391b60263a0d15bb2abefa8c9d
John <jwinkler@John-SLS.(none)>
2013-03-11 00:24:58 -0400

    Prepare for the viewing of a creatives market listing

--- application/views/pages/admin/markets.php
+++ application/views/pages/admin/markets.php
@@ -1,5 +1,14 @@
 <div id="cp_main" class="grid_16">
-	<ul>
-		<li><a href="admin/markets/metrolist">List all metros</a></li>
-	</ul>
+<!--	<ul>
+		li><a href="admin/markets/metrolist">List all metros</a></li>
+	</ul>-->
+<br /> <br />
+<h3> Current Market's For Creative</h3>
+<br />
+
+<table id="tblCurMarkets"></table>
+<br />
+
+
+<?php echo '<br/>' . $form . '<br />'; ?>
 </div>
