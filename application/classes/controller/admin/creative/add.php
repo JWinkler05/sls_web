451614623af4a7263a57e16e6aada6e4a5c4de5d
Thomas Cooper <tcooper@tcooper-VirtualBox.(none)>
2013-03-17 15:54:57 -0400

    fixed add creative

--- application/classes/controller/admin/creative/add.php
+++ application/classes/controller/admin/creative/add.php
@@ -38,17 +38,21 @@ class Controller_Admin_Creative_Add extends Controller_Template_Cityscape_Defaul
 			// Remove submit field if present in the post variables
 			$fields = array ('fields' => $posted);
 			unset($fields['fields']['submit']);
+			
+			$fields['fields']['ad_size'] = 1;
 
 			// Execute put request to create record
-			//$request = Request::factory("http://$api_server/creatives/{$detail->creative_id}")
-			//	->method(Request::PUT)
-			//	->body(json_encode($fields))
-			//	->headers('Content-Type', 'application/json');
+			$request = Request::factory('http://'.Servers::$api_server.'/creatives')
+				->method(Request::POST)
+				->body(json_encode($fields))
+				->headers('Content-Type', 'application/json');
 		
-			//$response = $request->execute();
+			$response = $request->execute();
+			
+			$creative = json_decode($response);
 
 			// Parse response for creative id
-			$creative_id = 'abc';
+			$creative_id = $creative->results->creative->creative_id;
 
 			// Redirect to edit page
 			$this->request->redirect('/admin_creative_edit?id='.$creative_id); 
