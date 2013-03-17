d8ffbbf93bf1c0c8f233933514a2219434cf9f0d
John <jwinkler@John-SLS.(none)>
2013-03-17 15:41:47 -0400

    Updated market page to gather the current markets and a ddl of markets it doesnt have yet.

--- application/classes/controller/admin/creative/markets.php
+++ application/classes/controller/admin/creative/markets.php
@@ -18,31 +18,51 @@ class Controller_Admin_Creative_Markets extends Controller_Template_Cityscape_De
                 
 		$form = Formo::form();
 
-		$markets_request = $this -> _get_markets($creative_id);//json_decode(Request::factory('api_markets_get/index')->query(array('id' => $id))->execute());
-                $marketArr = array();
-                
+		$markets_request_byid = $this -> _get_markets_by_creative($creative_id);//json_decode(Request::factory('api_markets_get/index')->query(array('id' => $id))->execute());
+                $markets_request = $this -> _get_markets();
+		$marketArr = array();
+                $marketArrAll = array();
 		//var_dump($markets_request);die();
 		
-                foreach($markets_request['results'] as $market)
+                foreach($markets_request_byid['results'] as $market)
                 {
                     $marketArr[$market['market']['id']] = $market['market']['market_name'];
                 }
+		
+		foreach($markets_request['results'] as $market)
+		{
+			$marketArrAll[$market['market']['id']] = $market['market']['market_name'];
+		}
                 
                 //Reset the pointer
                 reset($marketArr);
-                
-                $form->add_group('market_select', 'select', $marketArr, key($marketArr), array('label' => 'Market Choice'));
-
-                
+                reset($marketArrAll);
+		
+		if(count($marketArr) > 0){
+			$marketArrAll = $this -> _adjust_market_list($marketArrAll, $marketArr);
+		}
+		
+                $form->add_group('market_select', 'select', $marketArrAll, key($marketArrAll), array('label' => 'Market Choice'));
+		
+		$view-> tblMarket = $marketArr;
+		
 		if ($form->load()->validate())
 		{
 			// Get the market ID they want to change to.
 			$market_id = $form-> market_select->val();
-                        die('STOP');
-                        //JAW 2/17/2013 - Changed to get array, then parse for file path and full id
-			$this -> _update_creative_market($creative_id, $market_id);
-                        
-			$form = NULL;
+			
+			if(!key_exists($market_id, $marketArr))
+			{
+				//JAW 2/17/2013 - Changed to get array, then parse for file path and full id
+				$this -> _update_creative_market($creative_id, $market_id);
+				die('DEATH');
+				//http_redirect('admin/creative_markets?id='.$creative_id);
+			}
+			else
+			{
+				var_dump($market_id); die('IN - STOP');
+			}
+
 		}
                 
                 $form->add('Update', array('type' => 'submit','css' => array('id' => 'btnUpload')));
@@ -57,19 +77,51 @@ class Controller_Admin_Creative_Markets extends Controller_Template_Cityscape_De
 		$this->template->content = $view;
         }
  
-        protected function _get_markets($creative_id)
+        protected function _get_markets_by_creative($creative_id)
         {
-            // Execute put request to update record
-			$request = Request::factory('http://'.Servers::$api_server."/markets/$creative_id")
+            // Execute get to find the markets assigned to a creative
+			$request = Request::factory('http://'.Servers::$api_server."/creatives/$creative_id/markets")
 				->method(Request::GET)
 				->headers('Content-Type', 'application/json');
 		
 			return json_decode($request->execute(),true);
 
         }
+	
+	protected function _get_markets()
+        {
+            // Execute request to get all valid markets
+			$request = Request::factory('http://'.Servers::$api_server."/markets/")
+				->method(Request::GET)
+				->headers('Content-Type', 'application/json');
+			$markets = json_decode($request->execute(),true);
+			
+			return json_decode($request->execute(),true);
+			
+
+        }
+	
+	protected function _adjust_market_list($baseArr, $fieldArr){
+	
+		foreach($fieldArr as $tempItem)
+		{
+			var_dump($tempItem);
+			
+			if(in_array($tempItem, $baseArr)){
+				unset($baseArr[array_search($tempItem, $fieldArr)]);
+			}
+		}
+		return $baseArr;
+	}
         
 	protected function _update_creative_market($creative_id, $market_id)
         {
-            return 1;
+		
+		// Execute request to get all valid markets
+			$request = Request::factory('http://'.Servers::$api_server."/markets/$creative_id/markets/$market_id")
+				->method(Request::PUT)
+				->headers('Content-Type', 'application/json');
+		
+			return json_decode($request->execute(),true);
         }
 }
--- application/views/pages/admin/markets.php
+++ application/views/pages/admin/markets.php
@@ -6,9 +6,14 @@
 <h3> Current Market's For Creative</h3>
 <br />
 
-<table id="tblCurMarkets"></table>
-<br />
-
+<?php 
+		foreach($tblMarket as $market)
+		{
+		$temp = array_keys($tblMarket, $market);
+		echo $market;
+		echo '<a href="creative_market_delete?market_id= ' .$temp[0].'"> Delete</a> <br />';
+		}
+?>
 
 <?php echo '<br/>' . $form . '<br />'; ?>
 </div>
