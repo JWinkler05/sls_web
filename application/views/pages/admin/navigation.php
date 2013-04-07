0b3d1792202dd3d9f50fb61077686737e8c12306
John <jwinkler@John-SLS.(none)>
2013-04-07 13:50:52 -0400

    Fix Navigation. Show Nav menu when given ID, else Main Menu only.

--- application/views/pages/admin/navigation.php
+++ application/views/pages/admin/navigation.php
@@ -1,6 +1,27 @@
+
 <div id="cp_main" class="grid_16">
+	<br/>
 	<ul>
-		<li><a href="#">Go Back</a></li>
-		<li><a href="#">Do stuff</a></li>
+		<!-- JAW 4/7/2013 - Navigation -->
+		<?php
+		if (isset($_GET['id']))
+			{
+			$creative_id = $_GET['id'];
+		echo '<li><div>';
+		echo	'<div class="">' ; 
+		echo	"<a href='creative_edit?id=$creative_id'>Edit</a> |"; 
+		echo	"<a href='creative_images?id=$creative_id'>Images</a> |"; 
+		echo	"<a href='creative_markets?id=$creative_id'>Markets</a> |"; 
+		echo	"<a href='creative_categories?id=$creative_id'>Categories</a> | ";
+		echo	"<a href='creative_tags?id=$creative_id'>Tags</a> | ";
+		echo	"<a href='creative_delete?id=$creative_id'>Delete</a> ";
+		echo	'</div>';
+		echo '</div></li>'; 
+		
+		echo '<li><a href="/admin/Creatives">Go Back To Creatives</a></li>';	
+		}
+		?>
+		<li><a href="/admin/">Main Menu</a></li>
 	</ul>
+	<br/>
 </div>
