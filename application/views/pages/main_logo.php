a3221e5621d86a3634a8deb9e5676dc46383a321
Thomas Cooper <tcooper@tcooper-VirtualBox.(none)>
2013-04-07 18:30:05 -0400

    made main image a logo

--- application/views/pages/main_logo.php
+++ application/views/pages/main_logo.php
@@ -1,7 +1,8 @@
-<?php echo HTML::image(
+<?php echo HTML::anchor('',
+	HTML::image(
 	'media/tpl-cityscape/img/slshigreslogo.png', 
 	array(
 		'id' => 'header_logo',
 		'alt' => 'smartlocalsocial',
 	)
-);?>
+));?>
