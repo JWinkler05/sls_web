0693e818a11fe1e7cb04989623b133f44ed519d2
John <jwinkler@John-SLS.(none)>
2013-04-14 20:39:31 -0400

    category delete page.

---
+++ application/views/pages/admin/creative/category/delete.php
@@ -0,0 +1,10 @@
+c<div id="cp_main_content" class="grid_16 no-sidebar">
+	<div class="grid_6">
+		<br /><p>Command Status: </p><br/>
+		<?php
+		echo("Creative: " .$creative_id. ":<br />");
+		echo("The market with ID " . $category_id . " has been removed.");
+		echo('<br /><br /> <a href = "/admin/creative_categories?id=' .$creative_id . '"> Go Back </a>');
+		?>
+	</div>
+</div>
