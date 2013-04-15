6ab5d42ac282091e3f883f1bdb1556e24aa89d2b
John <jwinkler@John-SLS.(none)>
2013-04-14 20:40:19 -0400

    Fix category page so it now shows current and delete option

--- application/views/pages/admin/creative/categories.php
+++ application/views/pages/admin/creative/categories.php
@@ -1,13 +1,18 @@
 <div id="cp_main" class="grid_16">
-<!--	<ul>
-		li><a href="admin/markets/metrolist">List all metros</a></li>
-	</ul>-->
+
 <br /> <br />
 <h3> Current Categories For Creative</h3>
 <br />
 
 <?php 
-	echo "This page is currently under construction."
+		foreach($tblCategory as $Category)
+		{
+		$temp = array_keys($tblCategory, $Category);
+		echo $Category;
+		$tempURL ='creative_category_delete?creative_id= ' .$creative_id. '&category_id= ' .$temp[0];
+		echo '<a href="' .$tempURL. '"> Delete </a> <br />';
+		}
 ?>
 
+<?php echo '<br/>' . $form . '<br />'; ?>
 </div>
