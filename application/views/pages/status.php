33e06430beb6b3306b14f26c134696e337fd9680
John <jwinkler@John-SLS.(none)>
2013-05-18 00:08:23 -0400

    Add in future piece for other types of users

--- application/views/pages/status.php
+++ application/views/pages/status.php
@@ -3,8 +3,12 @@ if (Auth::instance()->logged_in()) {
 	echo HTML::anchor('session/logout','Logout',array('class'=>'status_button status_right'));
 	echo 'Welcome '.Auth::instance()->get_user()->email.': '; 
 	if (Auth::instance()->logged_in('admin')) { 
-		echo HTML::anchor('admin','Control Panel',array('class'=>'status_button'));
+		echo HTML::anchor('admin','Admin Control Panel',array('class'=>'status_button'));
 	}
+	//JAW - 5/17/2013 - Comment out until user auth and interface complete.
+//	elseif (Auth::instance()->logged_in('member')) { 
+//		echo HTML::anchor('admin','My Account',array('class'=>'status_button'));
+//	}
 } else {
 	//JAW 4/21/2013 - issue 19 -- Add register button.
 	echo HTML::anchor('session/registration','Register',array('class'=>'status_button status_right'));
