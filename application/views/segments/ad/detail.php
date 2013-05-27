ca846936ca83dacdd6f1b13897b7c4efd8583b1c
John <jwinkler@John-SLS.(none)>
2013-05-27 17:49:56 -0400

    Add in link handeling for image, twitter, and facebook links.

--- application/views/segments/ad/detail.php
+++ application/views/segments/ad/detail.php
@@ -2,9 +2,39 @@
     <div class="innerwhite">
 	<div class="mainimg grid_8">
 	    <center><h1 class="buistitle"><?php echo $selected->business_name; ?></h1></center>
-	     <a href="http://<?php echo $selected->website; ?>" alt="Website URL">
-	    <img width="450" height="328" alt="image" src="<?php echo "/media/ad_images/$detail_image" ?>">
+	    <?php if($selected->website != NULL) { ?> 
+	     <?php if(strtolower(substr($selected->website, 0, 4)) != 'http') {
+			echo "<a href=\"http://$selected->website\" alt=\"WebsiteURL\" >";
+		} else {
+			echo "<a href=\"$selected->website\" alt=\"WebsiteURL\" >" ;
+		 }
+	     ?>
+	     <img width="450" height="328" alt="image" src="<?php echo "/media/ad_images/$detail_image" ?>">
+	     <?php if($selected->website != NULL) { ?>
 	     </a>
+	     <?php } } elseif($selected->facebook != NULL) { ?>
+	    <?php if(strtolower(substr($selected->facebook, 0, 4)) != 'http') {
+			echo "<a href=\"http://$selected->facebook\" alt=\"WebsiteURL\" >";
+		} else {
+			echo "<a href=\"$selected->facebook\" alt=\"WebsiteURL\" >" ;
+		 }
+	     ?>
+	     <img width="450" height="328" alt="image" src="<?php echo "/media/ad_images/$detail_image" ?>">
+	     <?php if($selected->facebook != NULL) { ?>
+	     </a>
+	    <?php } } elseif($selected->twitter != NULL) { ?>
+	    <?php if(strtolower(substr($selected->twitter, 0, 4)) != 'http') {
+			echo "<a href=\"http://$selected->twitter\" alt=\"WebsiteURL\" >";
+		} else {
+			echo "<a href=\"$selected->twitter\" alt=\"WebsiteURL\" >" ;
+		 }
+	     ?>
+	     <img width="450" height="328" alt="image" src="<?php echo "/media/ad_images/$detail_image" ?>">
+	     <?php if($selected->twitter != NULL) { ?>
+	     </a>
+	    <?php } } else { ?>
+	    <img width="450" height="328" alt="image" src="<?php echo "/media/ad_images/$detail_image" ?>">
+	    <?php } ?>
 	</div>
 	<div class="column grid_4 detail_offset">
 	    <p align="center"><?php echo $selected->description; ?></p>
@@ -23,6 +53,26 @@
 	    <br/>
 <!--	    <p><strong>Website</strong></p>
 	    <a href="http://<?php echo $selected->website; ?>" alt="Website URL"><?php echo $selected->website; ?></a>-->
+	    <br />
+	    <?php if($selected->facebook != NULL){ ?>
+	    <a href="<?php if(strtolower(substr($selected->facebook, 0, 4)) != 'http') {
+		    echo "http://" . $selected->facebook; ?>
+		    <?php } else {
+		    echo $selected->facebook; ?>
+			<?php } ?>
+		 "alt="Facebook Site">
+		 <img src="/media/tpl-cityscape/icons/facebook.png" height ="50" width="50" alt="FacebookPic"> </a>
+	    <?php } ?>
+		    
+	    <?php if($selected->twitter != NULL){ ?>
+	    <a href="<?php if(strtolower(substr($selected->twitter, 0, 4)) != 'http') {
+		    echo "http://" . $selected->twitter; ?>
+		    <?php } else {
+		    echo $selected->twitter; ?>
+			<?php } ?>
+		 "alt="Twitter Site">
+		 <img src="media/tpl-cityscape/icons/twitter.png" height ="50" width="50" alt="TwitterPic"> </a>
+	    <?php } ?>
 	</div>
     </div>
 </div>
