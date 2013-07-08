3180b318892e5903ec9d43c18aabcd3684a1fabf
John <jwinkler@John-SLS.(none)>
2013-07-07 20:28:24 -0400

    Add directional text to guide users home.

--- application/views/pages/main_logo.php
+++ application/views/pages/main_logo.php
@@ -1,8 +1,10 @@
-<?php echo HTML::anchor('',
+<?php 
+echo HTML::anchor('',
 	HTML::image(
 	'media/tpl-cityscape/img/slshigreslogo.png', 
 	array(
 		'id' => 'header_logo',
 		'alt' => 'smartlocalsocial',
 	)
-));?>
+));
+echo "<p style=\"font-size:10px; text-align:center;\"> Click the logo to return to the homepage </p>";	?>
