8c79f673bf8ce1b371d3262acc587baef208ecbf
Dev from web1 <web1.smartlocalsocial.com>
2013-07-10 03:02:29 +0000

    changed menu wording

--- application/views/pages/main_menu.php
+++ application/views/pages/main_menu.php
@@ -1,13 +1,13 @@
 <?php
 //JAW 4/21/2013 - issue 14 -- Change to match database category names.
 $navigation_links = array(
-	'' => 'all deals',
+	'' => 'home',
 	'?category=Nationwide' => 'nationwide deals',
 	'?category=Entertainment' => 'entertainment',
 	'?category=Dining%20And%20Food' => 'food and dining',
 	'?category=Health%20And%20Beauty' => 'health and beauty',
 	'?category=Travel%20And%20Hotel' => 'travel and hotel',
-	'?category=Home%20Service' => 'home services',
+	'?category=Home%20Service' => 'services',
 );
 ?>
 <ul>
