7f276b1fd14a9a57521f1a82b7b5874b8bd2ade0
John <JWinkler05@gmail.com>
2013-10-10 22:58:45 -0400

    Change text on link as per Bernie.

--- application/views/pages/market.php
+++ application/views/pages/market.php
@@ -1,2 +1,2 @@
-<div class="note"><a class="note-link" href="http://www.getdeals.mobi" target="_blank">Feature Of The Day</a></div>
+<div class="note"><a class="note-link" href="http://www.getdeals.mobi" target="_blank">Opt-In!</a></div>
 <div class="market-intro"><?php Request::factory('select/index')->execute();?></div>
